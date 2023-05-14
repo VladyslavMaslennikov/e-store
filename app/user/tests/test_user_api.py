cfa529ba157da67ec2455351aabd2fbea900800c
Vladyslav <vladyslav.maslennikov@tallium.com>
2023-05-14 15:31:28 +0300

    user api created

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -134,11 +134,11 @@ class PrivateUserApiTests(TestCase):
 
     def test_update_user_profile(self):
         """Test updating the user profile for the authenticated user."""
-        payload = {'name': 'Updated name', 'password': 'newpassword123'}
+        payload = {'name': 'Updated name', 'password': 'newpassword1234'}
 
         res = self.client.patch(ME_URL, payload)
 
         self.user.refresh_from_db()
-        self.assertNotEqual(self.user.name, payload['name'])
+        self.assertEqual(self.user.name, payload['name'])
         self.assertTrue(self.user.check_password(payload['password']))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
